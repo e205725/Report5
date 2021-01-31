bb1c4f143483b06717f4b469aaa505ae34acc45b
Namiki Teruya <e205725@ie.u-ryukyu.ac.jp>
2021-01-31 16:52:36 +0900

    1st commit

---
+++ Parselnt.java
@@ -0,0 +1,7 @@
+public class Parselnt {
+        public static void main(String[] args){
+            String str ="百十三";
+            int value;
+            value = Integer.parseInt(str);
+        }    
+}
